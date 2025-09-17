97ff170e97b727ad00db5900829b3d1c5cde56fa
Pratik Sharma <pratikvai9734@gmail.com>
2025-09-17 20:10:51 +0530

    changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter, Playfair_Display } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
